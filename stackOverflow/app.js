0dee4fee64b43d68cd34a23a4c761fb410c2863f
AntonioSalazar <antonio.salazar.emrsn@gmail.com>
2019-01-19 13:23:07 -0600

    in progress

--- stackOverflow/app.js
+++ stackOverflow/app.js
@@ -49,10 +49,13 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
+// const authRoutes = require("./routes/auth-routes");
+// app.use("/", authRoutes)
 
 const index = require('./routes/index');
 app.use('/', index);
+
+
 
 
 module.exports = app;
---
+++ stackOverflow/routes/auth-routes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const authRoutes  = express.Router();
+
+const User = require("../models/user");
